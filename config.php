52a4b26ea56af01da6a9a28326ce1dae91dd988a
schavan6 <schavan6@student.gsu.edu>
2022-09-13 18:46:04 -0400

    update config

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-echo "db " . var_export($cleardb_url);
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
+//$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+//echo "db " . var_export($cleardb_url);
+$cleardb_server = 'us-cdbr-east-06.cleardb.net';
+$cleardb_username = 'b4eef75396cc08';
+$cleardb_password = '887a5e18';
+$cleardb_db = 'heroku_60b60956fe00952';
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
