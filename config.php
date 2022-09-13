ea2d5fd03b8c8dc4e943f156babef80d6212bb3e
schavan6 <schavan6@student.gsu.edu>
2022-09-13 18:38:54 -0400

    update config

--- config.php
+++ config.php
@@ -1,16 +1,19 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'sonal');
-define('DB_PASSWORD', 'sonal');
-define('DB_NAME', 'store3');
- 
-/* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+echo "db " . var_export($cleardb_url);
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$link = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
  
 // Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+if ($link === false) {
+    #die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
